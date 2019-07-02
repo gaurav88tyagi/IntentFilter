66f5f6625a2ed2c76d1c73849f154f05538b808f
Gaurav Tyagi <gtyagi88cse@gmail.com>
2019-07-03 03:15:00 +0530

    Browsing using Anko library

--- app/src/main/java/com/example/intentfilter/MainActivity.kt
+++ app/src/main/java/com/example/intentfilter/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.browse
 import org.jetbrains.anko.toast
 import java.net.URI
 
@@ -18,18 +19,21 @@ class MainActivity : AppCompatActivity() {
 
         btnBrowse.setOnClickListener {
 
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(etWebUrl.text.toString()))
+//            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(etWebUrl.text.toString()))
+//
+//            try {
+//
+//                startActivity(intent)
+//
+//            } catch (anfe: ActivityNotFoundException) {
+//
+//                Log.e("ERROR", "Could not open Activity with given URL", anfe)
+//                toast("Could not find any app to open this link")
+//
+//            }
 
-            try {
+              browse(etWebUrl.text.toString())
 
-                startActivity(intent)
-
-            } catch (anfe: ActivityNotFoundException) {
-
-                Log.e("ERROR", "Could not open Activity with given URL")
-                toast("Could not find any app to open this link")
-
-            }
         }
     }
 }
